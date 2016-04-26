f41889e2e6dc6c2fdd6e94acef34869b5d57749e
Artur Schaefer <schaefer@imac8.bsinf>
2016-04-26 10:54:53 +0200

    add musterloesung

--- SpinLock/src/SpinLock.java
+++ SpinLock/src/SpinLock.java
@@ -41,3 +41,65 @@ public class SpinLock implements Lock{
 
 }
 
+/*
+ // Blatt 1, Aufgabe 1a-d - Muster Lösung
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+
+public class SpinLock implements Lock{
+	// false - not locked
+	// true - locked
+	private AtomicBoolean isLocked;
+
+	public SpinLock(){
+		isLocked = new AtomicBoolean(false);
+	}
+
+	public void lock(){
+		while(true){
+	    	if(isLocked.compareAndSet(false, true))
+	    		break;
+	    }
+	}
+
+	public void unlock(){
+		isLocked.compareAndSet(true, false);
+	}
+
+	@Override
+	public boolean tryLock() {
+		if(isLocked.compareAndSet(false, true)){
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+		long end = System.nanoTime() + unit.toNanos(time);
+		while(System.nanoTime() < end){
+			if(isLocked.compareAndSet(false, true)){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public Condition newCondition() {
+		// Muss nicht implementiert werden.
+		return null;
+	}
+
+	@Override
+	public void lockInterruptibly() throws InterruptedException {
+		// Muss nicht implementiert werden.
+	}
+
+}
+ * */
+
