43b0c2b41d6eba1cc3d4b1a33315d9a74a58445b
Artur Schaefer <schaefer@imac8.bsinf>
2016-04-26 10:25:49 +0200

    initial commit

---
+++ SpinnLock/src/SpinLock.java
@@ -0,0 +1,44 @@
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+
+public class SpinLock implements Lock{
+
+	ReentrantLock lock = new ReentrantLock();
+	
+	public SpinLock(){}
+
+	public void lock(){
+		lock.lock();
+	}
+
+	public void unlock(){
+		lock.unlock();
+	}
+
+	@Override
+	public boolean tryLock() {
+		return lock.tryLock();
+	}
+
+	@Override
+	public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+		return lock.tryLock(time, unit);
+	}
+
+	@Override
+	public Condition newCondition() {
+		// Muss nicht implementiert werden.
+		return null;
+	}
+
+	@Override
+	public void lockInterruptibly() throws InterruptedException {
+		// Muss nicht implementiert werden.
+	}
+
+}
+
